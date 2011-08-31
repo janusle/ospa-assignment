e2e55794eb1e41b9fa128636a6b6b6f2f5b444b0
Janus Le <janusle@gmail.com>
2011-09-01 00:55:00 +1000

    finish part of utility

--- makefile
+++ makefile
@@ -1,9 +1,16 @@
 CC=gcc
 FLAGS=-ansi -pedantic
-all:part1
+all:part1 part2
 
 part1: 
-	$(CC) -o part1 part1.c 
+	$(CC) -o part1 part1.c $(FLAGS)
+
+part2: error.o
+	$(CC) -o part2 part2.c error.o $(FLAGS)
+
+error.o:
+	$(CC) -c error.c $(FLAGS)
 
 clean:
-	-rm part1
+	-rm part1 part2
+	-rm *.o
--- part2.c
+++ part2.c
@@ -1,13 +1,46 @@
 #include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
 
-#define ARVMIN 1
+#include "error.h"
 
+#define ARVMIN 3
 
 int 
 main( int argc , const char** argv )
 {
+   pid_t pid;
+   int fd0[2], fd1[2];
+
    if( argc < ARVMIN )
+   {
+     usage();
+     exit(EXIT_FAILURE);
+   }
 
+   if( (pid = fork() ) < 0 )
+     err_quit("fork error\n");
+   
+   if( pipe(fd0) < 0 )
+     err_quit("pipe error\n");
 
-   return EXIT_FAILURE;
+   if( pipe(fd1) < 0 )
+     err_quit("pipe error\n");
+
+   if( pid == 0 )  /* process 1 */
+   {
+      close(fd0[0]); /* close read end */
+    
+      if( dup2(fd[1], STDOUT_FILENO) == -1 )
+      {
+        err_quit("dup error\n"); 
+      }
+
+   }
+   else
+   {
+      
+   }
+
+   return EXIT_SUCCESS;
 }
